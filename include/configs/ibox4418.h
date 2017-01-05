5e37a45a4f111f1f08c7025bb95f2307ff5b711e
Charleye <wangkart@aliyun.com>
2017-01-05 16:12:33 +0800

    Add new configuration for ibox4418 board

--- include/configs/ibox4418.h
+++ include/configs/ibox4418.h
@@ -1,4 +1,14 @@
+/*
+ * Charleye, wangkart@gmail.com
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ *
+ */
+
 #ifndef __CONFIG_H
 #define __CONFIG_H
+
+#define CONFIG_SYS_TEXT_BASE        (0x42C00000)
+#define CONFIG_SYS_INIT_SP_ADDR     (CONFIG_SYS_TEXT_BASE)
 
 #endif
