e18857723e8b94e2099549f8c6c358fddc9e2bc8
Charleye <wangkartx@gmail.com>
2017-02-06 15:26:30 +0800

    Create Clock configuration

---
+++ arch/arm/mach-s5p4418/clock.c
@@ -0,0 +1,13 @@
+/*
+ * Charleye <wangkartx@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ *
+ */
+
+#include <common.h>
+#include <asm/arch/clock.h>
+
+void clock_cpu_init(void)
+{
+}
---
+++ arch/arm/mach-s5p4418/include/mach/clock.h
@@ -0,0 +1,6 @@
+#ifndef _S5P4418_CLOCK_H
+#define _S5P4418_CLOCK_H
+
+
+void clock_cpu_init(void);
+#endif
