39a5c8a810680385f76b8218ab7edbfda74a89cf
Charleye <wangkart@aliyun.com>
2017-02-21 20:33:24 +0800

    modify

---
+++ arch/arm/mach-s5p4418/gpio.c
@@ -0,0 +1,22 @@
+/*
+ * Charleye <wangkartx@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ *
+ */
+
+#include <common.h>
+#include <asm/io.h>
+#include <asm/gpio.h>
+
+void gpio_set_pull(int gpio, int mode)
+{
+}
+
+void gpio_set_drv(int gpio, int mode)
+{
+}
+
+void gpio_cfg_pin(int gpio, int cfg)
+{
+}
