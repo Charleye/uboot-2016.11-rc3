cfd3205a8acc5215d18c0cb80663689baec54c0e
Charleye <wangkart@aliyun.com>
2017-02-23 13:43:07 +0800

    Add gpio configuration

--- arch/arm/mach-s5p4418/gpio.c
+++ arch/arm/mach-s5p4418/gpio.c
@@ -9,6 +9,25 @@
 #include <asm/io.h>
 #include <asm/gpio.h>
 
+#define CFG_MASK(val)           (0x3 << ((val) << 1))
+#define CFG_SFR(gpio, cfg)      ((cfg) << ((gpio) << 1))
+
+static void s5p4418_gpio_cfg_pin(struct s5p4418_gpio_bank *bank, int gpio, int cfg)
+{
+    unsigned int value;
+
+    if (gpio >> 4) {
+        value = readl(&bank->altfn1);
+        value &= ~CFG_MASK(gpio % 16);
+        value |= CFG_SFR(gpio % 16, cfg);
+        writel(value, &bank->altfn1);
+    } else {
+        value = readl(&bank->altfn0);
+        value &= ~CFG_MASK(gpio);
+        value |= CFG_SFR(gpio, cfg);
+        writel(value, &bank->altfn0);
+    }
+}
 void gpio_set_pull(int gpio, int mode)
 {
 }
