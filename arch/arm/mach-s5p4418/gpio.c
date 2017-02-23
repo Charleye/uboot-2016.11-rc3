0520502341a883d5877af5dd64bc2e382dbea715
Charleye <wangkart@aliyun.com>
2017-02-23 14:12:35 +0800

    GPIO config

--- arch/arm/mach-s5p4418/gpio.c
+++ arch/arm/mach-s5p4418/gpio.c
@@ -11,6 +11,10 @@
 
 #define CFG_MASK(val)           (0x3 << ((val) << 1))
 #define CFG_SFR(gpio, cfg)      ((cfg) << ((gpio) << 1))
+#define CFG_SFR_UNSHIFT(val, gpio)  ((val) >> ((gpio) << 1))
+
+#define DAT_MASK(gpio)          (0x1 << (gpio))
+#define DAT_SET(gpio)           (0x1 << (gpio))
 
 static void s5p4418_gpio_cfg_pin(struct s5p4418_gpio_bank *bank, int gpio, int cfg)
 {
@@ -28,6 +32,43 @@ static void s5p4418_gpio_cfg_pin(struct s5p4418_gpio_bank *bank, int gpio, int c
         writel(value, &bank->altfn0);
     }
 }
+
+static unsigned int s5p4418_gpio_get_cfg(struct s5p4418_gpio_bank *bank, int gpio)
+{
+    unsigned int value;
+
+    if (gpio >> 4) {
+        value = readl(&bank->altfn1);
+        value &= CFG_MASK(gpio % 16);
+        value = CFG_SFR_UNSHIFT(value, gpio % 16);
+    } else {
+        value = readl(&bank->altfn0);
+        value &= CFG_MASK(gpio);
+        value = CFG_SFR_UNSHIFT(value, gpio);
+    }
+
+    return value;
+}
+
+static void s5p4418_gpio_set_value(struct s5p4418_gpio_bank *bank, int gpio, int en)
+{
+    unsigned int value;
+
+    value = readl(&bank->out);
+    value &= ~DAT_MASK(gpio);
+    if (en)
+        value |= DAT_SET(gpio);
+    writel(value, &bank->out);
+}
+
+static unsigned int s5p4418_gpio_get_value(struct s5p4418_gpio_bank *bank, int gpio)
+{
+    unsigned int value;
+
+    value = readl(&bank->out);
+    return !!(value & DAT_MASK(gpio));
+}
+
 void gpio_set_pull(int gpio, int mode)
 {
 }
